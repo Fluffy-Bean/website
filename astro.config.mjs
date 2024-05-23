4fc0ee9d9c70dad557cbd2aadb99854c06405d1b
Fluffy-Bean <michal-gdula@protonmail.com>
2024-05-23 20:34:54 +0100

    :raccoon:

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://Fuffy-Bean.github.io',
-    // base: '/website',
+    base: '/website',
 });
