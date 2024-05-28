54e79beb307256848afe43a484367b4830bf1e64
Fluffy-Bean <michal-gdula@protonmail.com>
2024-05-28 21:36:29 +0100

    Add MDX support Start writing a blog and clean up/hide tester stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import syntaxTheme from "./syntax-theme.json";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
-    markdown: {
-        syntaxHighlight: "shiki",
-        shikiConfig: {
-            theme: syntaxTheme,
-        },
-    },
+  output: "static",
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: syntaxTheme
+    }
+  },
+  integrations: [mdx()]
 });
